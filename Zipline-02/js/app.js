a0b24ee82fbf38d4ecb17c1f8c0dabab4ff4cb3a
LawlietBlack <kenneth.black@live.com>
2015-09-01 17:39:00 -0700

    finished quotes machine

--- Zipline-02/js/app.js
+++ Zipline-02/js/app.js
@@ -1,12 +1,35 @@
 $(document).ready(function() { 
+	//gets quotes from my own API, https://github.com/LawlietBlack/quotes-api
 	$('#get-quotes').click(function(event) {
 		console.log(event)
 		function callback(data) {
 			$('#quote').html(" ");
-			var $author = $('<h1 class="text-center">').html(data.author);
-			var $text = $('<p class="text-center">').html(data.text);
+			var $author = $('<h1 class="text-center" id="author">').html(data.author);
+			var $text = $('<p class="text-center" id="text">').html(data.text);
 			$('#quote').append($author).append($text);
 		}
 		$.getJSON('http://198.199.95.142:7378/quote/random', callback)
-	})
+	});
+
+	//creates a popup tweet window populated with the quote
+  $('.popup').click(function(event) {
+  	if($('#text').html() && $('#text').html()) {
+  		var quote = "\""+ $('#text').html() + "\" - " + $('#author').html();
+  	} else {
+  		var quote = "";
+  	}
+  	// console.log(encodeURI(quote))
+  	var width  = 575,
+      height = 400,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      url    = this.href,
+      opts   = 'status=1' +
+               ',width='  + width  +
+               ',height=' + height +
+               ',top='    + top    +
+               ',left='   + left;
+  	window.open(url + "?text=" + encodeURI(quote), 'twitter', opts);
+  	return false;
+  });
 });
