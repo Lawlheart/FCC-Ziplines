44c185d6a6087d5e3082c1619d6d8e43569650fd
LawlietBlack <kenneth.black@live.com>
2015-09-02 17:09:25 -0700

    refactored event handlers

--- Zipline-04/js/app.js
+++ Zipline-04/js/app.js
@@ -3,9 +3,10 @@ var clockRunning = false;
 var timeLeft;
 var sessionTime = 1500;
 var breakTime = 300;
-var seconds = sessionTime; // 1500 seconds for 25 minutes
 var breakClock = false;
+var seconds;
 
+//Converts seconds to MM:SS
 function filterTime(seconds) {
   var min = Math.floor(seconds/60);
   var sec = seconds%60;
@@ -15,6 +16,7 @@ function filterTime(seconds) {
     return min + ":" + sec;
   }
 }
+//renders the background fill effect with different colors for session, break, and pause
 function renderBackground() {
   var color = '#444'
   var timer = breakClock?breakTime:sessionTime;
@@ -24,6 +26,7 @@ function renderBackground() {
   var progress = (timer - seconds)*100/timer;
   $('#stopWatch').css('background', 'linear-gradient(to top, '+color+' 0%,'+color+' '+progress+'%,#222 '+progress+'%,#222 100%)');
 }
+//counts down till seconds = 0, then plays alarm and switches between session and break mode.
 function timer() {
   if(seconds > 0) {
     seconds -= 1;
@@ -34,27 +37,30 @@ function timer() {
     if(!breakClock) {
       seconds = breakTime;
       $('#title').html("Break");
-      $('#stopWatch').removeClass('session');
-      $('#stopWatch').addClass('break');
-
+      $('#stopWatch').removeClass('session').addClass('break');
       breakClock = true;
     } else {
       seconds = sessionTime;
       $('#title').html("Session");
-      $('#stopWatch').addClass('session');
-      $('#stopWatch').removeClass('break');
+      $('#stopWatch').addClass('session').removeClass('break');
       breakClock = false;
     }
   }
   renderBackground()
   $('#time').html(filterTime(seconds));
 }
+//renders time variables in the view with respect to break/session mode;
+function renderTime() {
+  seconds = breakClock?breakTime:sessionTime;
+  $('#time').html(filterTime(seconds));
+  $('#sessTime').html(Math.floor(seconds/60));
+  $('#brkTime').html(Math.floor(breakTime/60));
+}
 
-$('#time').html(filterTime(seconds));
-$('#sessTime').html(Math.floor(seconds/60));
-$('#brkTime').html(Math.floor(breakTime/60));
+//Initial page rendering
+renderTime()
 
-
+//add play/pause functionality to the stopwatch button
 $('#stopWatch').click(function() {
   if(!clockRunning) {
     timeLeft = setInterval(function() {timer()}, 1000);
@@ -68,48 +74,27 @@ $('#stopWatch').click(function() {
   renderBackground()
 });
 
+//reset button brings app back to session mode at full time.
 $('#reset').click(function() {
   seconds = sessionTime;
   $('#title').html("Session");
-  $('#stopWatch').addClass('session');
-  $('#stopWatch').removeClass('break');
+  $('#stopWatch').addClass('session').removeClass('break');
   $('#time').html(filterTime(seconds));
   breakClock = false;
   renderBackground()
 });
 
 
-$('#addSessTime').click(function() {
-  sessionTime += 60;
-  $('#sessTime').html(Math.floor(sessionTime/60));
-  if(!breakClock) {
-    seconds = sessionTime;
-    $('#time').html(filterTime(seconds));
+$('body').on('click', '.time-select', function(e) {
+  var method = e.target.getAttribute("data-method");
+  switch(method) {
+    case "add-sess": sessionTime += 60; break;
+    case "sub-sess": sessionTime -= 60; break;
+    case "add-break": breakTime += 60; break;
+    case "sub-break": breakTime -= 60; break;
   }
-});
-$('#subSessTime').click(function() {
-  sessionTime -= 60;
-  $('#sessTime').html(Math.floor(sessionTime/60));
-  if(!breakClock) {
-    seconds = sessionTime;
-    $('#time').html(filterTime(seconds));
-  }
-});
-$('#addBrkTime').click(function() {
-  breakTime += 60;
-  $('#brkTime').html(Math.floor(breakTime/60));
-  if(breakClock) {
-    seconds = breakTime;
-    $('#time').html(filterTime(seconds));
-  }
-});
-$('#subBrkTime').click(function() {
-  breakTime -= 60;
-  $('#brkTime').html(Math.floor(breakTime/60));
-  if(breakClock) {
-    seconds = breakTime;
-    $('#time').html(filterTime(seconds));
-  }
+  renderTime()
+
 });
 
 });
