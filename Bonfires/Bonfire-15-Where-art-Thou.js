4b99168a17503a66c8540114c3f0cfa746194e09
LawlietBlack <kenneth.black@live.com>
2015-09-06 20:44:06 -0700

    Completed bonfires 43-46 and revised Bonfire 15

--- Bonfires/Bonfire-15-Where-art-Thou.js
+++ Bonfires/Bonfire-15-Where-art-Thou.js
@@ -2,15 +2,19 @@ function where(collection, source) {
   var arr = [];
   // What's in a name?
   for(var i=0;i<collection.length;i++) {
+    var match = true;
     for(key in source) {
-      if(collection[i].hasOwnProperty(key)) {
-        if(source[key] === collection[i][key]) {
-          arr.push(collection[i])
-        }
+      console.log(!collection[i].hasOwnProperty(key), source[key] !== collection[i][key])
+      if(!collection[i].hasOwnProperty(key) || source[key] !== collection[i][key]) {
+         match = false;
       }
     }
+    if(match) {
+      arr.push(collection[i])
+    }
   }
+  console.log(arr)
   return arr;
 }
 
-where([{ first: 'Romeo', last: 'Montague' }, { first: 'Mercutio', last: null }, { first: 'Tybalt', last: 'Capulet' }], { last: 'Capulet' });
+where([{ 'a': 1, 'b': 2 }, { 'a': 1 }, { 'a': 1, 'b': 2, 'c': 2 }], { 'a': 1, 'b': 2 })
